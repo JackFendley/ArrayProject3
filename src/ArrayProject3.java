893c26dd0afab3f6094557326ac02d6940fef6a1
JackFendley <jackfendley18@gmail.com>
2016-11-30 10:44:15 -0600

    User Inputs 5 numbers and the program will display the largest number back
    User Inputs 5 numbers and the program will display the largest number
    back

---
+++ src/ArrayProject3.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author j.fendley
+ *
+ */
+public class ArrayProject3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner userInput = new Scanner(System.in);
+		
+		int[] userNum = new int [5];
+		int largestNum = 0;
+		
+		System.out.println("Please enter 5 whole numbers integer values: ");
+		
+		for(int i = 0; i < userNum.length; i++){
+			System.out.print("Please enter a number: ");
+			userNum[i] = userInput.nextInt();
+		}
+		
+		largestNum = userNum[0];
+		
+		for(int i = 0; i < userNum.length; i++){
+			
+			if(userNum[i] > largestNum){
+				largestNum = userNum[i];
+			}
+			
+		}
+		
+		System.out.print("The largest number you entered is " + largestNum);
+		
+	}
+
+}
